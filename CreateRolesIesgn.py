e59ebff0bd8a9e0f9673ed8971cd14913e3b4c3f
José Domingo Muñoz <josedom24@gmail.com>
2022-01-23 21:30:46 +0100

    Create roles

---
+++ CreateRolesIesgn.py
@@ -0,0 +1,30 @@
+from pm_gn import *
+import sys
+pgn=ConectarProxmox()
+roles=[{"nombre":"iesgn","privs":"VM.Clone, VM.Allocate, VM.Config.CPU, Pool.Audit, VM.Snapshot, VM.Config.HWType, \
+     VM.Config.Network, VM.Config.CDROM, Sys.Audit, VM.Config.Disk, Sys.Syslog, Sys.Console, Datastore.AllocateSpace,\
+     VM.Config.Memory, VM.Backup, VM.Config.Options, VM.PowerMgmt, VM.Console, VM.Migrate, Datastore.Audit, Sys.Modify,\
+     VM.Config.Cloudinit, Permissions.Modify, VM.Audit, VM.Monitor, Datastore.Allocate"},\
+    {"nombre":"iesgn-template-clone","privs":"Pool.Audit, VM.Clone, VM.Audit"}
+    ]
+# Creo los roles del IESGN
+for rol in roles:
+    try:
+        pgn.access.roles.create(roleid=rol["nombre"],privs=rol["privs"])
+    except:
+        print("Ya está creado el rol:",rol["nombre"])
+
+# Creo el pool imagenes
+
+try:
+    pgn.pools.create(poolid="Imagenes")
+except:
+    print("Pool imágenes ya creado")
+
+# Asigno a los grupos que hay el rol iesgn-template-clone, para el pool imágenes
+
+for grupo in GetGrupos(pgn):
+    try:
+        pgn.access.acl.set(path="/pool/Imagenes",roles="iesgn-template-clone",groups=grupo)
+    except:
+        print("Ya está asignado el rol iesgn-template-clone al grupo:",grupo)
